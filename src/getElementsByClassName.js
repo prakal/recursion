21c1af2dc7c40e032d323993c688f5b1c79c5dcb
Prakal <pranav.89@gmail.com>
2015-03-01 02:03:05 -0500

    made some progress in getElementsByClass. could refactor to make it better though.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,6 +5,30 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
-){
+,element){
   // your code here
+  if (element==undefined){
+  	var children=document.body.childNodes;
+  }
+  else{
+  	var children=element.childNodes;
+  }
+  //can use each or reduce here, think about it...
+  for (var i=0;i<children.length;i++){
+  	console.log(children[i],children[i].childNodes.length,children[i].classList);
+  	if (_.indexOf(children[i].classList,className)!=-1){
+
+  	}
+  	if (children[i].childNodes.length>0){
+  		getElementsByClassName(className,children[i]);
+  	}
+  }
+  // console.log(className);
+  // console.log(children);
+  // if (children.length>1){
+  // 	getElementsByClassName(className,children);
+  // }
+  // for (childNodes in document.body){
+  // 	console.log(childNodes);
+  // }
 };
